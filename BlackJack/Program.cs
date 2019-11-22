8416cf2630ade702b2b409c23647434b4efb2e1d
yamamoto_yuta <40594324+yuta-yamamoto54@users.noreply.github.com>
2019-11-22 21:49:51 +0900

    ブラックジャック初めての作成

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,6 +10,127 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
+            List<int> cards = new List<int>();
+            List<int> cpu_cards = new List<int>();
+            List<int> player_cards = new List<int>();
+            int player_score = 0;
+            int cpu_score = 0;
+            (string mark, int number) card_name;
+            int card;
+            string player_choice = "y";
+            Random rand = new Random();
+
+            for (int i = 0; i < 52; i++)
+            {
+                cards.Add(i);
+            }
+            
+            for (int i = 0; i < 2; i++)
+            {
+                card = cards[rand.Next(cards.Count())];
+                cards.Remove(card);
+                cpu_cards.Add(card);
+            }
+            card_name = GetCardName(cpu_cards[0]);
+            Console.WriteLine("ディーラーの引いた一枚目のカード：{0}の{1}", card_name.mark,card_name.number);
+            Console.WriteLine("ディーラーの引いた二枚目のカード：？？？");
+
+            for (int i = 0; i < 2; i++)
+            {
+                card = cards[rand.Next(cards.Count())];
+                cards.Remove(card);
+                player_cards.Add(card);
+            }
+            card_name = GetCardName(player_cards[0]);
+            Console.WriteLine("プレイヤーの引いた一枚目のカード：{0}の{1}", card_name.mark, card_name.number);
+            card_name = GetCardName(player_cards[1]);
+            Console.WriteLine("プレイヤーの引いた二枚目のカード：{0}の{1}", card_name.mark, card_name.number);
+
+            while (player_choice == "y") {
+                Console.WriteLine("カードを引きますか？y/n");
+                player_choice = Console.ReadLine();
+
+                if (player_choice == "y")
+                {
+                    card = cards[rand.Next(cards.Count())];
+                    cards.Remove(card);
+                    player_cards.Add(card);
+                    card_name = GetCardName(card);
+                    Console.WriteLine("プレイヤーの引いたカード：{0}の{1}", card_name.mark, card_name.number);
+                }
+                player_score = GetScore(player_cards);
+                Console.WriteLine("あなたの現在の得点は{0}です",player_score);
+
+                if (player_score >= 21 || player_choice == "n") break;
+
+            }
+            cpu_score = GetScore(cpu_cards);
+            Console.WriteLine("ディーラーの現在の得点は：{0}", cpu_score);
+
+            while (cpu_score < 17)
+            {
+                card = cards[rand.Next(cards.Count())];
+                cards.Remove(card);
+                cpu_cards.Add(card);
+                card_name = GetCardName(card);
+                Console.WriteLine("ディーラーの引いたカード：{0}の{1}", card_name.mark, card_name.number);
+                cpu_score = GetScore(cpu_cards);
+                Console.WriteLine("ディーラーの現在の得点は：{0}", cpu_score);
+            }
+
+            if (player_score > 21)
+            {
+                if (cpu_score > 21)
+                {
+                    Console.WriteLine("引き分け");
+                }
+                else
+                {
+                    Console.WriteLine("ディーラーの勝利");
+                }
+            }
+            else
+            {
+                if (cpu_score > 21)
+                {
+                    Console.WriteLine("プレイヤーの勝ち");
+                }
+                else if (cpu_score < player_score)
+                {
+                    Console.WriteLine("プレイヤーの勝ち");
+                }
+                else
+                {
+                    Console.WriteLine("ディーラーの勝ち");
+                }
+            }
+        
+            Console.ReadLine();
+        }
+
+        static public (string mark,int number) GetCardName(int card)
+        {
+            string mark = "";
+            int tmp = card / 13;
+            if (tmp == 0) { mark = "ハート"; }
+            else if (tmp == 1) { mark = "ダイヤ"; }
+            else if (tmp == 2) { mark = "エース"; }
+            else if (tmp == 3) { mark = "クローバー"; }
+
+            int number = 1 + card % 13;
+            return (mark, number);
+        }
+
+        static public int GetScore(List<int> hand_cards)
+        {
+            int sum_score = 0;
+            for (int i = 0; i < hand_cards.Count(); i++)
+            {
+                int score = hand_cards[i] % 13;
+                if (score >= 10) score = 10;
+                sum_score = sum_score + (score + 1);
+            }
+            return sum_score;
         }
     }
 }
